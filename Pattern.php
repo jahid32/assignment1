2b78d846015f9b8c8b563fb2f2d6504512017e8c
Md  Mostafizur Rahman <jahid32@gmail.com>
2024-06-11 02:20:26 +0000

    Updated Pattern Code

--- Pattern.php
+++ Pattern.php
@@ -1,7 +1,13 @@
 <?php
 
 $numbers = readline("Enter a number: ");
-
-for($i=0; $i>=$numbers; $i++){
-    
+$numbers = intval($numbers);
+if($numbers < 0){
+    $numbers = $numbers * -1;
+}
+$totalStar = $numbers * 2 -1;
+for($i=0; $i<$numbers; $i++){
+    $step =  1+$i*2;
+    $blankspace = ($totalStar - $step) / 2;
+    echo str_repeat(' ', $blankspace) . str_repeat('*', $step) . str_repeat(' ', $blankspace) . PHP_EOL;
 }
