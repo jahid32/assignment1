256ef0985388cc0770ffd042d318bca9fa4c64c9
Md  Mostafizur Rahman <jahid32@gmail.com>
2024-06-11 02:35:01 +0000

    Updated some of digit

--- ReverseString.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$string = readline("Enter a Sentance:  ");
-$string = explode(' ', $string);
-foreach ($string as $key => $word) {
-   $string[$key] = strrev($word);
-}
-echo implode(' ', $string);
--- SomeOfDigit.php
+++ SomeOfDigit.php
@@ -1,6 +1,20 @@
 <?php
 
 $numbers = readline("Enter a number: ");
-
+$negativ = false;
 $numbers = intval($numbers);
+if($numbers < 0){
+    $numbers = $numbers * -1;
+    $negativ = true;
+}
+
+$sum = 0;
+while($numbers > 0){
+    $digit = $numbers % 10;
+    $sum+=$digit;
+    $numbers = intval($numbers / 10);
+}
+
+echo $negativ ? $sum *-1 : $sum;
+
 
